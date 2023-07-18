867d8284899c3fbb2e1e4c08eda4474a68312eea
PETER IRUNGU <peteralidante254@gmail.com>
2023-07-18 14:35:27 +0300

    access chatGPT via api

--- src/main/java/com/aldgpt/App.java
+++ src/main/java/com/aldgpt/App.java
@@ -1,5 +1,15 @@
 package com.aldgpt;
 
+import java.io.IOException;
+
 public class App {
-      
+    public static void main(String[] args) {
+        try {
+            String prompt = "Write a Java class to find the factorial of a number.";
+            String response = GPTClient.getChatGptResponse(prompt);
+            System.out.println("ChatGPT Response:\n" + response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/aldgpt/GPTClient.java
+++ src/main/java/com/aldgpt/GPTClient.java
@@ -1,5 +1,17 @@
 package com.aldgpt;
 
+import java.io.IOException;
+import com.aldgpt.utils.HttpUtil;
+
 public class GPTClient {
-      
+
+      static String link = "https://api.openai.com/v1/engines/davinci-codex/completions";
+      static String api  = "";
+      private static final String CHATGPT_API_ENDPOINT = link;
+      private static final String API_KEY = api;
+
+      public static String getChatGptResponse(String prompt) throws IOException {
+            String requestBody = "{\"prompt\": \"" + prompt + "\", \"max_tokens\": 100}";
+            return HttpUtil.postRequest(CHATGPT_API_ENDPOINT, API_KEY, requestBody);
+      }
 }
